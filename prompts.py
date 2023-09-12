f394cf0ad579446c00ef6c50a4285f6d4c19aad0
Adam Stcklov <adamstecklov@Adams-MacBook-Pro.local>
2023-09-12 12:58:36 -0400

    deleted old prompts

--- prompts.py
+++ prompts.py
@@ -1,31 +1,3 @@
-# CREATE_ENDPOINT = """
-# Your goal is to create a Python FastAPI API endpoint.
-# You will return a json object with two elements:
-# "imports": python code of the imports section. Can be empty if not needed.
-# "endpoint": python code for the FastAPI API endpoint function, including the decorator.
-# It is essential that you include the FastAPI decorator.
-# Your response is going to be parsed automatically into a JSON object, so return nothing except the valid json.
-# Do not import FastAPI.
-
-# The name of the endpoint is {name}, the url path is {path}
-# The function takes in the following parameters: {inputs}
-# you can decide to use path or query parameters for the inputs.
-# the function outputs the following: {outputs}
-
-# This is the description of the endpoint function:
-# {functionality}
-# """
-
-
-# ON_CREATE_ERROR = """
-# Error when testing the function.
-# Review the original instructions, and modify your output based on the error:
-# {error}
-
-# Do not return anything except the JSON object with "imports" and "endpoint".
-# Do not include any apology message or other useless text.
-# """
-
 FUNCTION_CALLING_ENDPOINT_CREATION = {
     "name": "create_api",
     "description": "Create a new API endpoint and test it. If an error occurs during testing, the error will be returned to you.",
